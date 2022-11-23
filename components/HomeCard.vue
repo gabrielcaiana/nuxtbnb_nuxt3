ee7d272298647fa02904fdb088f7d5e13c256248
Gabriel Caiana <gabrielcaianaguedes@gmail.com>
2022-11-23 17:37:01 -0300

    feat: creating our homepage

---
+++ components/HomeCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  home: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <img :src="home.images[0]" style="width: 200px" :alt="home.title" /> <br />
+    {{ home.title }} <br />
+    {{ home.location.address }} {{ home.location.city }} {{ home.location.state
+    }}<br />
+    {{ home.guests }} guests <br />
+    {{ home.pricePerNight }} / night <br />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
-<script setup></script>
+<script setup>
+import homes from "~/data/homes";
 
+const getHomes = computed(() => homes.slice(0, 3));
+</script>
 
 <template>
-    <div>
-        hello world
-    </div>
+  <div style="display: flex; gap: 2rem">
+    <home-card v-for="home in getHomes" :home="home" :key="home.objectID" />
+  </div>
 </template>
