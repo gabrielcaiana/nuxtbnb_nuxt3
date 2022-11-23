a41b5a27b1088b4085505ba394aca61a4450f63b
Gabriel Caiana <gabrielcaianaguedes@gmail.com>
2022-11-23 18:17:22 -0300

    chore: how does routing in nuxt work

---
+++ pages/home/[id].vue
@@ -0,0 +1,17 @@
+<script setup>
+import homes from '~/data/homes.json'
+const route = useRoute()
+
+useHead({
+  title: "My home",
+});
+
+const home = computed(() => homes.find((home) => route.params.id === home.objectID))
+
+</script>
+
+<template>
+  <div>
+    {{ home.title }}
+  </div>
+</template>
