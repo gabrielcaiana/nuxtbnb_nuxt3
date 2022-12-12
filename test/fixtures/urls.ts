cf8bf076ea7e62f7d6359687953549694cb5223e
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-12-11 23:03:20 -0300

    fix: urls

--- test/fixtures/urls.ts
+++ test/fixtures/urls.ts
@@ -1,1 +1,4 @@
-export const appUrl = `${process.env.APP_URL}`;
+export const appUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://nuxtbnb-nuxt3.vercel.app";
