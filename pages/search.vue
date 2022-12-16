c508824a2f7cd70b24bb363da848e70b480a7a05
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-12-16 07:26:46 -0300

    chore: perform new api call when a query changes

--- pages/search.vue
+++ pages/search.vue
@@ -1,19 +1,30 @@
 <script setup>
 const route = useRoute();
 
-const { data: homes } = await $fetch("/api/algolia/get-home-by-location", {
-  params: {
-    lat: route.query.lat,
-    lng: route.query.lng,
-  },
-});
+const { data: homes, refresh } = await useFetch(
+  "/api/algolia/get-home-by-location",
+  {
+    query: {
+      lat: route.query.lat,
+      lng: route.query.lng,
+    },
+  }
+);
+
+watch(
+  () => route.query,
+  () => {
+    debugger;
+    refresh();
+  }
+);
 </script>
 
 <template>
   <div>
     {{ route.query.label }}<br />
     <ul>
-      <li v-for="home in homes" :key="home.id">{{ home.title }}</li>
+      <li v-for="home in homes.data" :key="home.id">{{ home.title }}</li>
     </ul>
   </div>
 </template>
